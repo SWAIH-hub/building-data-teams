669f66118e1b626703cc113075a77aef056f57bb
sam-vautier <samantha.vautier@somersetft.nhs.uk>
2025-06-20 16:02:10 +0100

    changes to render logo

--- current_team.py
+++ current_team.py
@@ -6,8 +6,6 @@ from functions import functions
 warnings.filterwarnings("ignore")
 
 # st.set_page_config(page_title="Budget Bytes", page_icon=":muscle:", layout="wide")
-st.logo("assets\SWAIH_logo.png", size="large", ) # SWAIH / Data Hub Logo 
-
 with open("style.css") as css:
     st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
 
